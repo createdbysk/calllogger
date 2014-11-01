7581fe3ef609d53ff3b1e500eddcdc6f46b4ac2f
Satish Kumar <kumarnotifications@gmail.com>
2014-10-31 22:24:17 -0500

    Add test for the default logger not called with no configuration.

--- test/callLogger.test.js
+++ test/callLogger.test.js
@@ -4,21 +4,34 @@ describe('callLogger', function () {
     var testFunction;
     var expectedReturnValue;
     var expect;
+    var logger;
+    var consoleLogSpy;
     beforeEach(function (done) {
         'use strict';
         callLogger = require('../callLogger');
         sinon = require("sinon");
         expect = require('expect.js');
+        // Setup the test function to return the expected value.
         testFunction = sinon.stub();
         expectedReturnValue = 42;
         testFunction.onFirstCall().returns(expectedReturnValue);
         testFunction.returns(undefined);
+        consoleLogSpy = sinon.spy(console, "log");
+        done();
+    });
+    afterEach(function (done) {
+        consoleLogSpy.restore();
         done();
     });
     it('should call function under test.', function (done) {
         var returnValue = callLogger(testFunction);
         sinon.assert.calledOnce(testFunction);
         expect(returnValue).to.be(expectedReturnValue);
+        done();
+    });
+    it('should not call the default logger, which is console.log, with no configuration.', function (done) {
+        var returnValue = callLogger(testFunction);
+        sinon.assert.notCalled(consoleLogSpy);
         done();
     });
 });
